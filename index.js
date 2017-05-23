b74d64912c13987d917d721dd6c7d74242b73651
Nicholas-Kyle <nickdotkyle@hotmail.com>
2017-05-23 12:01:27 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 // Returns true if a given string is a letter
 function isLetter(str) {
     var letter = /^[a-z]+$/;
@@ -38,73 +37,77 @@ var app = {
             var clientId;
             var nextWid;
             var prevWid;
+            var widArray = [];
+
             // checks salesId, password and clientId with current values, 
-            // returns true and sets values to variables if they are different
+            // returns true if they are different
             // returns false if they are the same
             function newCredentials() {
                 if (salesId != document.getElementById("salesId").value
                         || password != document.getElementById("password").value
                         || clientId != document.getElementById("clientId").value) {
-                    salesId = document.getElementById("salesId").value;
-                    password = document.getElementById("password").value;
-                    clientId = document.getElementById("clientId").value;
                     return true;
                 } else {
                     return false
                 }
             }
 
-            // STEP 1: checks salesId, password and clientId with current values
-            // if they are different procede STEP 2 else procede to STEP 7
-            // STEP 2: checks salesId is of a valid format
-            // STEP 2.1: stores salesId as variable salesId
-            // STEP 3: stores password as variable password
-            // STEP 4: stores clientId as variable clientId
-            // STEP 5: calls API to return all widgets details
-            // STEP 6: stores all widget IDs in an array and sets nextWid variable to 0
-            // and prevWid to size of array - 2
+            // sets salesId, password and clientId with current values
+            function setCredentials() {
+                salesId = document.getElementById("salesId").value;
+                password = document.getElementById("password").value;
+                clientId = document.getElementById("clientId").value;
+            }
+
+            // gets widget objects and adds widget ids to widArray
+            function getWidgetIds() {
+                widArray = [];
+                $.get('http://137.108.93.222/openstack/api/widgets/?OUCU=' + salesId + '&password=' + password,
+                        function (data) {
+                            var obj = JSON.parse(data);
+                            if (obj.status == "error") {
+                                alert(obj.message);
+                            } else {
+                                $.each(obj.data, function (i, widObj) {
+                                    widArray.push(widObj.id);
+                                });
+                                nextWid = 0;
+                                prevWid = widArray.length - 2;
+                                console.log(prevWid);
+                            }
+                        });
+            }
+
             // STEP 7: calls API to return widget details corresponding to nextWidget
             // STEP 8: displays widget image, description and asking price
-            // STEP 9: increments nextWidget and prevWidget values
-            //             
+            // STEP 9: increments nextWidget and prevWidget values 
 
             this.nextWidget = function () {
                 if (validUserFormat('salesId')) {
-                if (newCredentials()) {
-                    //get data, create and array of widget ids
-                    $.get('http://137.108.93.222/openstack/api/widgets/?OUCU=' + salesId + '&password=' + password,
-                        function (data) {
-                            alert( "Data Loaded: " + data );
-                        });
-                }
-                    
+                    if (newCredentials()) {
+                        setCredentials();
+                        getWidgetIds();
                         
-                        //    var obj = $.parseJSON(data);
-                        //    if (obj.status == "fail") {
-                        //    alert(obj.data[0].reason);
-                        //    } else {
-                        //    $.each(obj.data, function (index, value) {
-
-                            
-
-                            
-
-                            // calls API to return all widgets details
-                            // STEP 6: stores all widget IDs in an array and sets nextWid variable to 0
-                            // and prevWid to size of array - 2
+                        //    when printing info to html!!!!!!
+                        //          document.getElementById("subTotal").innerHTML = "out";
 
 
-                            else {
-                            console.log("same");
-                            }
-                        } else {
-                        alert("Sales ID incorrect format")
+                        // calls API to return all widgets details
+                        // STEP 6: stores all widget IDs in an array and sets nextWid variable to 0
+                        // and prevWid to size of array - 2
+
+
                     }
+                    // why is prevWid not set?
+                    console.log(prevWid);
+                } else {
+                    alert("Sales ID incorrect format")
+                }
 
-                }
-                ;
-                }
-                this.megaMaxSale = new MegaMaxSale();
             }
-        };
-        app.initialize();
+            ;
+        }
+        this.megaMaxSale = new MegaMaxSale();
+    }
+};
+app.initialize();
